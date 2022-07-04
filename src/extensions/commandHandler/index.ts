79589fb024ef372537183685f60a149bb923d56b
TrixiS <oficialmorozov@gmail.com>
2022-07-04 19:11:43 +0500

    Make command handler extenstion fit the new pattern

---
+++ src/extensions/commandHandler/index.ts
@@ -0,0 +1,80 @@
+import BotClient from "../../client";
+import BaseExtension from "../../lib/extension";
+import eventHandler from "../../lib/eventHandler";
+import { CommandInteraction, Interaction } from "discord.js";
+import { runCallbackName } from "../../lib/command";
+
+export default class CommandHandlerExtension extends BaseExtension {
+  constructor(client: BotClient) {
+    super(client);
+  }
+
+  @eventHandler("interactionCreate")
+  async commandsInteractionsHandler(interaction: Interaction) {
+    if (
+      (!interaction.isCommand() || !interaction.command) &&
+      !interaction.isContextMenu()
+    ) {
+      return;
+    }
+
+    const command = this.client.commands.get(interaction.commandName);
+
+    if (!command) {
+      return;
+    }
+
+    const runOptions = command.getRunOptions(interaction);
+    const data = await command.getData(interaction);
+    const runHandler = command.handlers.find(
+      (handler) => handler.name === runCallbackName
+    );
+
+    if (!(interaction instanceof CommandInteraction)) {
+      if (!runHandler) {
+        return;
+      }
+
+      return await runHandler.callback.call(command, runOptions, data);
+    }
+
+    const [subcommandGroup, subcommandName] =
+      this.getSubcommandData(interaction);
+
+    if (!subcommandName) {
+      if (!runHandler) {
+        return;
+      }
+
+      return await runHandler.callback.call(command, runOptions, data);
+    }
+
+    const subcommandHandler = command.handlers.find(
+      (handler) =>
+        handler.group === subcommandGroup && handler.name === subcommandName
+    );
+
+    await subcommandHandler?.callback.call(command, runOptions, data);
+  }
+
+  getSubcommandData(interaction: CommandInteraction) {
+    let name: string | undefined;
+    let group: string | undefined;
+
+    try {
+      name = interaction.options.getSubcommand();
+    } catch (e) {
+      name = undefined;
+    }
+
+    if (name) {
+      try {
+        group = interaction.options.getSubcommandGroup();
+      } catch (e) {
+        group = undefined;
+      }
+    }
+
+    return [group, name];
+  }
+}
