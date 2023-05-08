03c66c656ae2498fdc23cae0bc57a033d10d5053
TrixiS <oficialmorozov@gmail.com>
2023-05-08 22:24:57 +0300

    Refactor state

--- src/utils/state.ts
+++ src/utils/state.ts
@@ -1,53 +1,35 @@
-// TODO: generic state manager with adapters for redis, sqlite3
-
 import { eventHandlerCheckFactory } from "@trixis/lib-ts-bot";
 import { ClientEvents } from "discord.js";
 
-export class StateContext<
-  State extends string = string,
-  Data extends Record<string, any> = Record<string, any>
-> {
-  public state?: State;
-  public data: Partial<Data> = {};
+export type StateCtx<
+  TState extends string = string,
+  TData extends Record<string, any> = Record<string, any>
+> = {
+  state?: TState;
+  data?: Partial<TData>;
+};
 
-  constructor(
-    private readonly manager: typeof StateManager,
-    public readonly userId: string
-  ) {}
-
-  public clear() {
-    this.manager.clearState(this.userId);
-  }
-}
-
+// TODO: pass adapter: Memory, Prisma, Redis
 export class StateManager {
-  private static readonly contexts = new Map<string, StateContext>();
+  private static readonly contexts = new Map<string, StateCtx>();
 
-  public static getContext<
-    State extends string,
-    Data extends Record<string, any>
-  >(userId: string) {
-    const ctx = this.contexts.get(userId);
+  static getCtx<T extends StateCtx>(userId: string): T {
+    let ctx = this.contexts.get(userId) as T | undefined;
 
     if (ctx) {
-      return ctx as StateContext<State, Data>;
+      return ctx;
     }
 
-    const newCtx = new StateContext<State, Data>(this, userId);
-    this.contexts.set(userId, newCtx);
-
-    return newCtx;
+    ctx = {} as T;
+    this.contexts.set(userId, ctx);
+    return ctx;
   }
 
-  public static clearState(userId: string) {
-    return this.contexts.delete(userId);
-  }
-
-  public static setState<
-    State extends string,
-    Data extends Record<string, any> = Record<string, any>
-  >(userId: string, state: State) {
-    const ctx = this.getContext<State, Data>(userId);
+  static setState<T extends StateCtx>(
+    userId: string,
+    state: T extends StateCtx<infer TState> ? TState : never
+  ) {
+    const ctx = this.getCtx<T>(userId);
     ctx.state = state;
     return ctx;
   }
@@ -60,8 +42,8 @@ export const stateCheckFactory =
   <State extends string>(state: State) =>
     eventHandlerCheckFactory<Event>(async (...args: ClientEvents[Event]) => {
       const userId = getUserIdCallback(...args);
-      const stateCtx = StateManager.getContext(userId);
-      return stateCtx.state === state;
+      const ctx = StateManager.getCtx(userId);
+      return ctx.state === state;
     });
 
 export const interactionStateCheck = stateCheckFactory<"interactionCreate">(
